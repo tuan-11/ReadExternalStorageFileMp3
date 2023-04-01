b0f531a8e079b0ce7dcca578ff06de2d56f8bc6e
Trần Quốc Tuấn <tranquoctuantqd2002@gmail.com>
2023-04-01 11:06:09 +0700

    update ReadExternalStorageFileMp3

---
+++ ExternalStorageFileMp3/app/src/main/java/hcmute/edu/vn/externalstoragefilemp3/TranQuocTuan20110595/MainActivity.java
@@ -0,0 +1,14 @@
+package hcmute.edu.vn.externalstoragefilemp3.TranQuocTuan20110595;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
